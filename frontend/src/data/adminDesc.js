62a827c059766461ed4e4acaa9f94a35ce395eee
Utkarsh Chauhan <113504261+utkarshchauhan26@users.noreply.github.com>
2025-07-11 17:55:57 +0530

    Update data source of adminDesc

---
+++ frontend/src/data/adminDesc.js
@@ -0,0 +1,22 @@
+/** @format */
+
+const Toparticles = [
+    {
+        title: "Product Manager",
+        date: "May 2025 ",
+        href: "#",
+    },
+];
+
+const AllArticles = {
+  
+  2025: [
+    {
+        title: "How to be a Pm",
+        date: "January 2025",
+        href: "https://dev.to/jaiminbariya/top-10-tech-podcasts-for-software-engineers-to-level-up-in-2025-518j",
+    },
+  ]
+};
+
+export { Toparticles, AllArticles };
