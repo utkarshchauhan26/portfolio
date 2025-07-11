08c0a87207741fa66d06a06ddc87c089a669b2de
Utkarsh Chauhan <113504261+utkarshchauhan26@users.noreply.github.com>
2025-07-11 23:51:29 +0530

    Update data source of herosData

---
+++ frontend/src/data/herosData.js
@@ -0,0 +1,14 @@
+const heroSection = {
+  welcome: "Welcome to my digital lab ", 
+  description: `
+  I’m a builder at heart—someone who blends tech, creativity, and product thinking to solve problems that *actually matter*.
+
+  I've worked across AI, ML, full-stack, and cloud—but my real edge? Turning raw ideas into smart, user-driven products that feel 10x better than what's out there.
+
+  I don’t just ship features. I obsess over the *why*, the *who*, and the *how*—until people say, “Damn, that’s clever.”
+
+  Right now, I’m carving my path into product management, not just to manage, but to lead with clarity, curiosity, and bold execution.
+
+  Let’s create products that matter — and make people say, ‘that’s brilliant.
+  `,
+};
